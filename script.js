05e615c0f35fca895fa0eebabf27d596b631ba7d
szwedzius <jakubszweda1@gmail.com>
2022-02-15 13:37:35 +0100

    Finishing basic js mechanics

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+id = document.getElementsByClassName('point').length;
+console.log(id);
+
+
+
 function remove(ID)
 {
     document.getElementById(ID).classList.remove('task');
     document.getElementById(ID).classList.add('taskdone');
+}
+
+function add()
+{
+    if(id%2==0)
+    {
+        document.getElementById("task_list").innerHTML += 
+        '<div class="col-6 col-md-4"><div class="point task" id="' + id +'" onclick="remove('+ id +')">'+document.getElementById("task_input").value+'</div></div>';
+    }
+    else
+    {
+        document.getElementById("task_list").innerHTML += 
+        '<div class="col-6 col-md-4 offset-md-4"><div class="point task rig" id="' + id +'" onclick="remove('+ id +')">'+document.getElementById("task_input").value+'</div></div>';
+    }
+
+    id = id + 1;
+    document.getElementById("task_input").value = "";
 }
