d2446c8425a20fa424ee55fe93da1de424e30231
szwedzius <jakubszweda1@gmail.com>
2022-02-16 10:42:34 +0100

    Basic error handling

--- script.js
+++ script.js
@@ -7,21 +7,34 @@ function remove(ID)
 {
     document.getElementById(ID).classList.remove('task');
     document.getElementById(ID).classList.add('taskdone');
+    if(document.getElementsByClassName('task').length==0){
+        window.alert("CONGRATULATIONS");
+    }
 }
 
 function add()
 {
-    if(id%2==0)
+    if(document.getElementById("task_input").value.length < 15)
     {
-        document.getElementById("task_list").innerHTML += 
-        '<div class="col-6 col-md-4"><div class="point task" id="' + id +'" onclick="remove('+ id +')">'+document.getElementById("task_input").value+'</div></div>';
+        if(id%2==0)
+        {
+            document.getElementById("task_list").innerHTML += 
+            '<div class="col-6 col-md-4"><div class="point task" id="' + id +'" onclick="remove('+ id +')">'+document.getElementById("task_input").value+'</div></div>';
+        }
+        else
+        {
+            document.getElementById("task_list").innerHTML += 
+            '<div class="col-6 col-md-4 offset-md-4"><div class="point task rig" id="' + id +'" onclick="remove('+ id +')">'+document.getElementById("task_input").value+'</div></div>';
+        }
+        id = id + 1;
+        document.getElementById("task_input").value = "";
     }
     else
     {
-        document.getElementById("task_list").innerHTML += 
-        '<div class="col-6 col-md-4 offset-md-4"><div class="point task rig" id="' + id +'" onclick="remove('+ id +')">'+document.getElementById("task_input").value+'</div></div>';
+        document.getElementById("err").innerHTML = "You need to give shorter input just to ensure that you aren't trying to break the site"
     }
 
-    id = id + 1;
-    document.getElementById("task_input").value = "";
+    
+
+    
 }
